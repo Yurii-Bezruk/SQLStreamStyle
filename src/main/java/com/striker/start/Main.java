5b31d588b73479d245b2768ee2f068e2ab5252df
Yurii Bezruk <yurii.bezruk.it@gmail.com>
2021-08-24 20:38:16 +0300

    Qualified TableMapper class. Now it generates java code simpler

--- src/main/java/com/striker/start/Main.java
+++ src/main/java/com/striker/start/Main.java
@@ -4,23 +4,24 @@ import com.striker.sqlstream.SQLStream;
 
 import java.util.List;
 import java.util.Map;
-import static com.striker.tableproxies.Tables.*;
+
 
 public class Main {
     public static void main(String[] args) {
-        List<Map<String, Object>> result;
-        SQLStream stream = SQLStream
-                .SELECT(user_with_dep.name, department.name, country.name)
-                .FROM(user_with_dep)
-                .RIGHT_JOIN(department)
-                .ON("user_with_dep.department = department.id")
-                .RIGHT_JOIN(country)
-                .ON("department.country = country.id")
-                .WHERE("country.id > 1")
-                .GROUP_BY(country.name.ASC())
-                .ORDER_BY(department.name.DESC(), country.name.ASC());
-        System.out.println(stream.toFormattedString());
-        result = stream.execute();
-        result.forEach(System.out::println);
+//        List<Map<String, Object>> result;
+//        SQLStream stream = SQLStream
+//                .SELECT(user_with_dep.name, department.name, country.name)
+//                .FROM(user_with_dep)
+//                .RIGHT_JOIN(department)
+//                .ON("user_with_dep.department = department.id")
+//                .RIGHT_JOIN(country)
+//                .ON("department.country = country.id")
+//                .WHERE("country.id > 1")
+//                .GROUP_BY(country.name.ASC())
+//                .ORDER_BY(department.name.DESC(), country.name.ASC());
+//        System.out.println(stream.toFormattedString());
+//        result = stream.execute();
+//        result.forEach(System.out::println);
+        TableMapper.map();
     }
 }
--- src/main/java/com/striker/start/TableMapper.java
+++ src/main/java/com/striker/start/TableMapper.java
@@ -40,8 +40,7 @@ public class TableMapper {
 
     public static void map() {
         Map<String, List<String>> tableData = loadTableData();
-        createJava(tableData);
-        compileJava();
+        createMappedTablesJavaFile(tableData);
     }
 
     private static Map<String, List<String>> loadTableData() {
@@ -66,99 +65,30 @@ public class TableMapper {
         }
         return tables;
     }
+    private static void createMappedTablesJavaFile(Map<String, List<String>> tableData) {
+        try (PrintWriter writer = new PrintWriter( sourceFolder + "/com/striker/tableproxies/Tables.java")){
+            writer.print(
+                "package com.striker.tableproxies;\n\n" +
+                "public final class Tables {\n\n");
+            for (String table : tableData.keySet()) {
+                writer.printf("\tpublic static final %sTable %s = new %sTable();\n", table, table, table);
+            }
+            writer.println("\n\tprivate Tables(){}\n");
 
-    private static void createJava(Map<String, List<String>> tableData) {
-        JavaClassCreator creator = new JavaClassCreator("Tables", sourceFolder);
-        creator.packageName = "com.striker.tableproxies";
-        creator.classBody.constructors = new String[]{"private Tables(){}"};
-        List<JavaClassCreator.Class> nestedClasses = new ArrayList<>();
-        for (String table : tableData.keySet()) {
-            String nestedClassName = table + "Table";
-            JavaClassCreator.Class nestedClass = new JavaClassCreator.Class(nestedClassName);
-            nestedClass.superClassName = "Table";
-
-            nestedClass.fields =
-                    tableData.get(table).stream()
-                            .map(column -> "public final Column " + column + " = new Column(\"" + column + "\");")
-                            .toArray(String[]::new);
-
-            nestedClass.constructors = new String[]{
-                    "public "+nestedClassName+"() {\n" +
-                            "\t\t\tsuper(\""+table+"\");\n" +
-                            "\t\t}"};
-            nestedClasses.add(nestedClass);
-        }
-        creator.classBody.nestedClasses = nestedClasses.toArray(new JavaClassCreator.Class[0]);
-        creator.classBody.fields = tableData.keySet().stream().map(table ->
-                "public static final "+table+"Table "+table+" = new "+table+"Table();")
-                .toArray(String[]::new);
-        creator.createJavaFile();
-    }
-
-    private static void compileJava(){
-        try {
-            Runtime.getRuntime().exec("javac Table.java Tables.java");
-        } catch (IOException e) {
+            for (String table : tableData.keySet()) {
+                writer.printf("\tpublic static class %sTable extends Table {\n", table);
+                for (String column : tableData.get(table)) {
+                    writer.printf("\t\tpublic final Column %s = new Column(\"%s\");\n", column, column);
+                }
+                writer.printf(
+                    "\n\t\tpublic %sTable() {\n" +
+                    "\t\t\tsuper(\"%s\");\n" +
+                    "\t\t}\n" +
+                    "\t}\n", table, table);
+            }
+            writer.println("}");
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
-        }
-    }
-    private static class JavaClassCreator {
-        String path;
-        String packageName;
-        String[] imports;
-        Class classBody;
-
-        public JavaClassCreator(String className, String path) {
-            classBody = new Class(className);
-            this.path = path;
-        }
-
-        public void createJavaFile(){
-            try (PrintWriter writer = new PrintWriter( path + "/" +
-                    packageName.replace('.', '/') + "/" + classBody.className + ".java")){
-                writer.printf("package %s;%n%n", packageName);
-                writer.printf("public final class %s {%n", classBody.className);
-                for (String field : classBody.fields) {
-                    writer.println("\t"+field);
-                }
-                writer.println();
-                for (String constructor : classBody.constructors) {
-                    writer.println("\t"+constructor);
-                }
-                writer.println();
-                for (Class nestedClass : classBody.nestedClasses) {
-                    writer.printf("\tpublic static class %s extends %s {%n", nestedClass.className, nestedClass.superClassName);
-                    for (String field : nestedClass.fields) {
-                        writer.println("\t\t"+field);
-                    }
-                    writer.println();
-                    for (String constructor : nestedClass.constructors) {
-                        writer.println("\t\t"+constructor);
-                    }
-                    writer.println("\t}");
-                }
-                writer.println("\n}");
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            }
-        }
-
-        static class Class {
-            String className;
-            String superClassName;
-            String[] fields;
-            String[] methods;
-            String[] constructors;
-            Class[] nestedClasses;
-
-            public Class(String className) {
-                this.className = className;
-            }
-
-            @Override
-            public String toString() {
-                return "Class{}";
-            }
         }
     }
 }
